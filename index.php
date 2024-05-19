608c33cff846fa2265c0d1aeeac74abe8fea4200
João Rodrigues <johnnyrod4569@gmail.com>
2024-05-19 11:07:47 +0200

    chore: Add new line at end of file to satisfy linting software.

--- index.php
+++ index.php
@@ -1,57 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Task Manager</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Task Manager</title>
 
-	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
 
-	<!-- Link to stylesheet -->
-	<link rel="stylesheet" href=".\static\css\style.css">
+  <!-- Link to stylesheet -->
+  <link rel="stylesheet" href=".\static\css\style.css">
 
 </head>
 <body>
-	<div id="main-container">
-		<div id="form-wrapper">
+  <div id="main-container">
+    <div id="form-wrapper">
 
-			<!-- container for form -->
-			<form id="form" class="flex-wrapper">
-					<div class="user-input">
-						<input id="title" class="form-control" type="text" name="title" placeholder="Add task">
-					</div>
-					<div class="date-input">
-						<input id="date" class="form-control" type="date" name="date" placeholder="Add due date">
-					</div>
-					<div class="submit">
-						<input id="submit" class="submit-button" type="submit" value="Add Task">
-					</div>
-					
-			</form>
+      <!-- container for form -->
+      <form id="form" class="flex-wrapper">
+          <div class="user-input">
+            <input id="title" class="form-control" type="text" name="title" placeholder="Add task">
+          </div>
+          <div class="date-input">
+            <input id="date" class="form-control" type="date" name="date" placeholder="Add due date">
+          </div>
+          <div class="submit">
+            <input id="submit" class="submit-button" type="submit" value="Add Task">
+          </div>
+          
+      </form>
 
-			<!-- container for task list heading -->
-			<div>
-				<div class="task-wrapper flex-wrapper">
-					<div class="task-list-item-container heading-title">
-							<span>Task List</span>
-					</div>
-					<div id="create-button" class="submit">
-							
-					</div>
-			</div>
-			</div>
-			
-		</div>
+      <!-- container for task list heading -->
+      <div>
+        <div class="task-wrapper flex-wrapper">
+          <div class="task-list-item-container heading-title">
+              <span>Task List</span>
+          </div>
+          <div id="create-button" class="submit">
+              
+          </div>
+      </div>
+      </div>
+      
+    </div>
 
-		<!-- container for task list -->
-		<div id="list-wrapper"></div>
-			
-	</div>
+    <!-- container for task list -->
+    <div id="list-wrapper"></div>
+      
+  </div>
 
-	<!-- Fontawesome script -->
-	<script src="https://kit.fontawesome.com/e26a309e14.js" crossorigin="anonymous"></script>
+  <!-- Fontawesome script -->
+  <script src="https://kit.fontawesome.com/e26a309e14.js" crossorigin="anonymous"></script>
   
-	<!-- Taskmanager script -->
+  <!-- Taskmanager script -->
   <script type="module" src="./static/index/build_list.js"></script>
 
 </body>
